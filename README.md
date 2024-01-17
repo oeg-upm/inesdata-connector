b6b2358cbe62a103f947c63d111369ab7d83abe9
Juan Calero <jmcalero@gmv.com>
2024-01-17 13:46:45 +0100

    Initial commit
    Ultrabasic connector that does nothing, but compile and launch

---
+++ README.md
@@ -0,0 +1,15 @@
+# InesData Connector
+
+Dataspaces connector for the InesData project, based on the EDC dataspaces framework
+
+## Launching the connector
+
+- Compile:
+  ```
+  gradle base:build
+  ```
+
+- Launch:
+  ```
+  java -jar base/build/libs/connector-app.jar
+  ```
---
+++ base/build.gradle.kts
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2020, 2021 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *       Fraunhofer Institute for Software and Systems Engineering - added dependencies
+ *
+ */
+
+plugins {
+    `java-library`
+    id("application")
+    alias(libs.plugins.shadow)
+}
+
+dependencies {
+    implementation(libs.edc.boot)
+    implementation(libs.edc.connector.core)
+}
+
+application {
+    mainClass.set("org.eclipse.edc.boot.system.runtime.BaseRuntime")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    mergeServiceFiles()
+    archiveFileName.set("connector-app.jar")
+}

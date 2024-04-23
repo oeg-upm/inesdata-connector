7b6aa7726fc959381cbf60011cc79b14d5eef379
Juan Calero <jmcalero@gmv.com>
2024-04-23 15:07:58 +0200

    Monitor implementation logging to SLF4J
    In this way, both Jetty and EDC can be controled through logback.
    Right now, every monitor log goes through the same SLF4J logger. Possible
    future improvement.

--- launchers/connector/src/main/java/org/upm/inesdata/monitor/ConnectorMonitorExtension.java
+++ launchers/connector/src/main/java/org/upm/inesdata/monitor/ConnectorMonitorExtension.java
@@ -1,7 +1,5 @@
 package org.upm.inesdata.monitor;
 
-import org.eclipse.edc.spi.monitor.ConsoleMonitor;
-import org.eclipse.edc.spi.monitor.ConsoleMonitor.Level;
 import org.eclipse.edc.spi.monitor.Monitor;
 import org.eclipse.edc.spi.system.MonitorExtension;
 
@@ -10,6 +8,6 @@ public class ConnectorMonitorExtension implements MonitorExtension {
     @Override
     public Monitor getMonitor() {
         
-        return new ConsoleMonitor("", Level.INFO);
+        return new Slf4jMonitor();
     }
 }

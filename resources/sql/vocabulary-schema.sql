0aa7bf2572a0da9ab504d2748622f4e38cd400cb
GRUPOGMV\ssis <sasierra@gmv.com>
2024-06-05 09:44:16 +0200

    Error sintaxis vocabularios

--- resources/sql/vocabulary-schema.sql
+++ resources/sql/vocabulary-schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS edc_vocabulary
     created_at         BIGINT  NOT NULL,
     json_schema        JSON    DEFAULT '{}',
     name               VARCHAR NOT NULL,
-    category           VARCHAR NOT NULL
+    category           VARCHAR NOT NULL,
     PRIMARY KEY (id)
 );
 
